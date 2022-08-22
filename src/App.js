951b1cabcc06aaf74c515b3bdcdc08dc3409a00c
Juana Llanje <juana.lc@hotmail.es>
2022-08-21 22:05:37 -0500

    Installing React in the project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita <code>src/App.js</code> y guarda para recargar.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apremnde 
+          Learn React
         </a>
       </header>
     </div>
