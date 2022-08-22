7fef281c3949cae4277662472025cef4f48bc7c3
Juana Llanje <juana.lc@hotmail.es>
2022-08-21 22:59:30 -0500

    Adding CSS design

---
+++ src/components/CreateTodoButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../stylesheets/CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+    return (
+    <button className="CreateTodoButton">+</button>
+    );
+  }
+
+export { CreateTodoButton };
