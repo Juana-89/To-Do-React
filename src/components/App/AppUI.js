1ae70257ee59065864a27d5e1da4052b10a29a23
Juana Llanje <juana.lc@hotmail.es>
2022-08-22 20:23:34 -0500

    Coding in the components

---
+++ src/components/App/AppUI.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext/index';
+import { TodoCounter } from '../TodoCounter/index';
+import { TodoSearch } from '../TodoSearch/index';
+import { TodoList } from '../TodoList/index';
+import { TodoItem } from '../TodoItem/index';
+import { TodoForm } from '../TodoForm/index';
+import { CreateTodoButton } from '../CreateTodoButton/index';
+import { Modal } from '../Modal/index';
+
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {error && <p>Hubo un error...</p>}
+        {loading && <p>Cargando...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
