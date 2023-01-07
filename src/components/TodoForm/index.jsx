3fc20d0b9bf99779f023edf422177d9d421611cc
Jllanjech <juana.lc@hotmail.es>
2023-01-07 02:06:26 -0500

    Adding styles in the modal

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,14 +1,15 @@
-import { useState, useContext } from 'react';
-import { TodoContext } from '../TodoContext';
-import './TodoForm.css';
+import { useState, useContext } from "react";
+import { TodoContext } from "../TodoContext";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FaRegSave, FaRegTimesCircle } from "react-icons/fa";
+
 
 function TodoForm() {
-  const [newTodoValue, setNewTodoValue] = useState('');
-  const {
-    addTodo,
-    setOpenModal,
-  } = useContext(TodoContext);
-  
+  const [newTodoValue, setNewTodoValue] = useState("");
+  const { addTodo, setOpenModal } = useContext(TodoContext);
+
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
   };
@@ -22,29 +23,35 @@ function TodoForm() {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label>Escribe tu nuevo TODO</label>
-      <textarea
-        value={newTodoValue}
-        onChange={onChange}
-        placeholder="Visitar al abuelo el domingo al mediodía"
-      />
-      <div className="TodoForm-buttonContainer">
-        <button
-          type="button"
-          className="TodoForm-button TodoForm-button--cancel"
-          onClick={onCancel}
-          >
-          Cancelar
-        </button>
-        <button
-          type="submit"
-          className="TodoForm-button TodoForm-button--add"
-        >
-          Añadir
-        </button>
-      </div>
-    </form>
+    <Form
+      onSubmit={onSubmit}
+      style={{ width: "100%", borderRadius: '8px',maxWidth: "350px", padding: "33px 40px" }}
+      className="flex mb-2 bg-white"
+    >
+      <Form.Group className="mb-3">
+        <Form.Label className="text-black">
+          <h5>Write your new TODO</h5>
+        </Form.Label>
+        <InputGroup>
+          <Form.Control
+            as="textarea"
+            rows={4} 
+            aria-label="With textarea"
+            value={newTodoValue}
+            onChange={onChange}
+            placeholder="I'm going to visit my granpa this sunday"
+          />
+        </InputGroup>
+      </Form.Group>
+      <Form.Group className="mb-3">
+          <Button className="m-2" variant="primary" type="submit" size="lg">
+            <FaRegSave/>{" "}Add
+          </Button>
+          <Button className="m-2" variant="light" type="button" onClick={onCancel} size="lg">
+          <FaRegTimesCircle/>{" "}Cancel
+          </Button>
+      </Form.Group>
+    </Form>
   );
 }
 
