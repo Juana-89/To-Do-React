34ccb97c3cb891e1755ee8152b6922eb00a4e275
Jllanjech <juana.lc@hotmail.es>
2023-01-08 17:08:51 -0500

    Adding footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import { FaCopyright, FaHeart } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div class="card text-center fixed-bottom">
+      <div class="card-footer text-muted">
+        <FaCopyright /> Designed by Juana Llanje Chávez <FaHeart />
+      </div>
+    </div>
+  );
+}
+
+export { Footer };
